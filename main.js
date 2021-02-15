3af9c28e8c58c25f450811cc8e750abca9626b7e
fbado66 <fbado66@gmail.com>
2021-02-15 00:33:19 -0500

    reworking the nav bar

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 
-    let sandwichMenu = document.querySelector('.menu-btn')
-    let responsiveMenu = document.querySelector('.menu')
-    let menuIcon = document.querySelector('#side-menu-icon')
+//     let sandwichMenu = document.querySelector('.menu-btn')
+//     let responsiveMenu = document.querySelector('.menu')
+//     let menuIcon = document.querySelector('#side-menu-icon')
 
-sandwichMenu.addEventListener('click', () => {
-    responsiveMenu.classList.toggle('active')
-    menuIcon.classList.toggle('fa-times')
+// sandwichMenu.addEventListener('click', () => {
+//     responsiveMenu.classList.toggle('active')
+//     menuIcon.classList.toggle('fa-times')
+// })
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
+//     document.getElementById("navbar").className = "scroll-on-Y"
+//     document.getElementById('logo').className ='logo-black'
+//   } else {
+//     document.getElementById("navbar").className = "";
+//     document.getElementById('logo').className = ""
+
+//   }
+// }
+
+const navTogggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link')
+
+navTogggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav_open')
 })
 
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
-    document.getElementById("navbar").className = "scroll-on-Y"
-    document.getElementById('logo').className ='logo-black'
-  } else {
-    document.getElementById("navbar").className = "";
-    document.getElementById('logo').className = ""
-
-  }
-}
-
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav_open')
+    })
+})
 
 
 
